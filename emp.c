b220d07a291abc7ee7f8dbec09c3ba0e08ebf150
Glau cy <94122730+Glauu@users.noreply.github.com>
2023-09-27 09:42:15 -0300

    V. atual
    automatização da gravação em arquivo csv

--- emp.c
+++ emp.c
@@ -5,44 +5,55 @@
 
 typedef struct
 {
-    string contador;
-    string datar;
-    string croqui;
-    string nome;
-    string setor;
-    string datad;
-    string ass;
+    char* cont;
+    char* datar;
+    char* croqui;
+    char* nome;
+    char* setor;
+    char* datad;
+    char* ass;
 }
 re;
 
 int main (void)
 {
-    re emp [1];
-    emp [0].contador = "1";
-    emp [0].datar = "21 set 2023";
-    emp [0].croqui = "100201";
+    re emp [3];
+    emp [0].cont = "1";
+    emp [0].datar = "21 set 2024";
+    emp [0].croqui = "300381";
+    emp [0].nome = "Glaucy";
+    emp [0].setor = "sic";
+    emp [0].datad = "22 set 2023";
+    emp [0].ass = "Glaucy";
+
+    emp [1].cont = "2";
+    emp [1].datar = "21 set 2024";
+    emp [1].croqui = "100201";
+    emp [1].nome = "Fabio";
+    emp [1].setor = "si";
+    emp [1].datad = "22 set 2023";
+    emp [1].ass = "Glaucy";
+    
+    emp [0].cont = "3";
+    emp [0].datar = "12 set 2024";
+    emp [0].croqui = "300387";
     emp [0].nome = "Glaucy";
     emp [0].setor = "sic";
     emp [0].datad = "22 set 2023";
     emp [0].ass = "Glaucy";
 
 
-    FILE *file = fopen("Controle de empréstimo.csv", "a");
+    FILE *file = fopen("Controle de empréstimo.csv", "w");
 
     if (file == NULL)
     return 1;
 
-    fprintf(file, emp[0]);
-    fclose(file);
+    int j;
 
-    for (int i = 0; i < 3; i++)
+    for(j = 0; j < 2; j++)
     {
-        if (strcmp(emp[i].nome, "Glaucy") == 0)
-        {
-            printf("Encontrados: %s \n", emp[i].setor);
-            return 0;
-        }
+        fprintf(file,"%s %s %s %s %s %s %s\n", emp[j].cont, emp[j].datar, emp[j].croqui, emp[j].nome, emp[j].setor, emp[j].datad, emp[j].ass);
     }
-        printf("Não localizado \n");
-        return 1;
+    fclose(file);
 }
+
